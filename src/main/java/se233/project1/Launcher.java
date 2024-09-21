a87a8ed3cf2caaba68b79d7418d71769740b449a
MewKina <141310839+MewKina@users.noreply.github.com>
2024-09-21 22:13:15 +0700

    Update Launcher.java

--- src/main/java/se233/project1/Launcher.java
+++ src/main/java/se233/project1/Launcher.java
@@ -2,12 +2,21 @@ package se233.project1;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 
 public class Launcher extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    public void start(Stage stage) {
-
+public void start(Stage stage) throws Exception{
+        primaryStage = stage;
+        FXMLLoader fxmlLoader = new FXMLLoader(Launcher.class.getResource("PaneP1.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Picture Manipulator");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
