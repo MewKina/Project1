f105fca595a0a5b9261e405d99568f1b1430038b
MewKina <141310839+MewKina@users.noreply.github.com>
2024-09-29 15:43:58 +0700

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module se233.project1 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
+    requires java.desktop;
+    requires javafx.swing;
 
 
-    opens se233.project1 to javafx.fxml;
+    opens se233.project1.controller to javafx.fxml;
     exports se233.project1;
 }
